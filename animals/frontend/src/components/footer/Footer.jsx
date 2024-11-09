6f71004ccec2cf7c41a2432bdab38bfec887e969
Artem <artiebears@mail.ru>
2024-11-09 08:40:14 +0300

    deleted old front

--- animals/frontend/src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer className="footer mt-auto py-3 bg-body-tertiary">
-            <div className="container">
-                <span className="text-muted  d-block text-center">© {new Date().getFullYear()} AAA IT для Цифрового прорыва</span>
-            </div>
-        </footer>
-    )
-}
--- animals/frontend/src/components/reportButton/ReportButton.jsx
+++
@@ -1,6 +0,0 @@
-export const ReportButton = () => {
-    return (
-        <a href="https://t.me/yappyhelpbot?start=_" target="_blank" rel="noreferrer"  className="btn report-button">
-            Сообщить о дубликате</a>
-    )
-}
